ef1e6790b972ad339610490b1d64d2494f54d39f
gorimatyan <gorima102131@gmail.com>
2025-09-09 00:39:25 +0900

    いい感じにフェードアウトするように修正したった

--- src/app/matching/[match_id]/MatchingPage.tsx
+++ src/app/matching/[match_id]/MatchingPage.tsx
@@ -2,9 +2,16 @@
 import BackgroundLayout from "@/features/layout/components/BackgroundLayout"
 import { CharacterSpeech } from "@/features/characterSpeech/CharacterSpeech"
 import { DebateFaceoffBoard } from "@/features/matching/components/DebateFaceoffBoard/DebateFaceoffBoard"
+import { useEffect, useState } from "react"
 
 export const MatchingPage =
   (/**{ params }: { params: { match_id: string } }*/) => {
+    const [active, setActive] = useState(false)
+    useEffect(() => {
+      const t = setTimeout(() => setActive(true), 200)
+      return () => clearTimeout(t)
+    }, [])
+
     return (
       <BackgroundLayout className="flex flex-col justify-center">
         <DebateFaceoffBoard
@@ -12,6 +19,7 @@ export const MatchingPage =
           negativeUserName="相手のユーザー名"
           themeName="教育"
           topic="小学校からのプログラミング教育は必要か"
+          isActive={active}
         />
         <CharacterSpeech
           videoSrc="/assets/videos/aristotle/he_mans_facial_express_2.mp4"
