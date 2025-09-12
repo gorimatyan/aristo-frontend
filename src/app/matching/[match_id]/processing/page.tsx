c5ada2e3b34fe2385ae6aafb71c8547e2ab815d9
gorimatyan <gorima102131@gmail.com>
2025-09-13 03:17:37 +0900

    集計中のページを作成

---
+++ src/app/matching/[match_id]/processing/page.tsx
@@ -0,0 +1,5 @@
+import { ProcessingPage } from "./ProcessingPage"
+
+export default function Processing(/**{ params }: { params: { match_id: string } }*/) {
+  return <ProcessingPage /**params={params} */ />
+}
